f0e28c2cebd3cd5718a61bdf61a33b557b1d7f78
Hoang Hong Quan <lzhoang2302@gmail.com>
2025-01-06 15:52:57 +0700

    Use latest Chrome User Agents

--- Scripts/resource_fetcher.py
+++ Scripts/resource_fetcher.py
@@ -13,7 +13,7 @@ else:
 class ResourceFetcher:
     def __init__(self, headers=None):
         self.request_headers = headers or {
-            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
         }
         self.buffer_size = 16 * 1024
 
